088f31ac54bdb0da832f8363bfd81445ec059e92
Morgan Matney <morgan.matney89@gmail.com>
2020-09-27 19:21:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
 
   def self.create_from_collection(students_array)
     binding.pry
-    students_array.each {|student| Student.new(students_array)}
+    students_array.each {|student| student.new(students_array)}
   end
 
   def add_student_attributes(attributes_hash)
